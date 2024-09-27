720b51519b044bb49aa1616044273f5067bcfac1
yahiaomarab <yahiaomar746@gmail.com>
2024-09-27 12:40:55 +0300

    solved bugs shown in debug console

--- lib/features/splash/presentation/views/splash-view.dart
+++ lib/features/splash/presentation/views/splash-view.dart
@@ -6,7 +6,7 @@ import 'package:movie/core/utils/colors/colors.dart';
 import 'package:movie/core/utils/routing/routes.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
+  const SplashScreen({Key? key}) : super(key: key);
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
@@ -22,7 +22,6 @@ class _SplashScreenState extends State<SplashScreen>
   void initState() {
     super.initState();
     initRotatingAndScalingAnimation();
-    goToOnBoardingPage();
     _navigateBasedOnState();
   }
 
@@ -37,15 +36,15 @@ class _SplashScreenState extends State<SplashScreen>
     bool isOnboardingComplete = await CacheHelper.getBoardingMode() ?? false;
     bool isUserLoggedIn = await CacheHelper.getUid() != null;
 
-    // Decide which route to navigate to
+    // Navigate based on the state of onboarding and login
     if (isOnboardingComplete && isUserLoggedIn) {
-      // Navigate to layout if both onboarding and login are complete
+      // Navigate to main layout if both onboarding and login are complete
       Navigator.pushReplacementNamed(context, Routes.layoutPath);
     } else if (!isOnboardingComplete) {
       // Navigate to onboarding if it's not complete
       Navigator.pushReplacementNamed(context, Routes.onBoardingPath);
     } else if (!isUserLoggedIn) {
-      // Navigate to login if user is not logged in
+      // Navigate to login if the user is not logged in
       Navigator.pushReplacementNamed(context, Routes.loginPath);
     }
   }
@@ -55,18 +54,18 @@ class _SplashScreenState extends State<SplashScreen>
     return Scaffold(
       backgroundColor: AppColors.buttonKColor,
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Container(
             padding: EdgeInsets.all(30.h),
-            height: MediaQuery.of(context).size.height * 0.9,
-            width: double.infinity,
             child: RotationTransition(
               turns: rotationAnimation,
               child: ScaleTransition(
                 scale: scaleAnimation,
                 child: Image.asset(
                   'assets/images/movieIcon.png',
+                  fit: BoxFit.contain,
                 ),
               ),
             ),
@@ -89,24 +88,14 @@ class _SplashScreenState extends State<SplashScreen>
       duration: const Duration(seconds: 2),
     );
 
-    rotationAnimation =
-        Tween<double>(begin: 0, end: 1).animate(animationController);
-    scaleAnimation = Tween<double>(begin: 0, end: 1).animate(
-      CurvedAnimation(
-        parent: animationController,
-        curve: Curves.easeInOut,
-      ),
-    );
+    rotationAnimation = Tween<double>(begin: 0, end: 1).animate(animationController);
+    scaleAnimation = Tween<double>(begin: 0, end: 1).animate(CurvedAnimation(
+      parent: animationController,
+      curve: Curves.easeInOut,
+    ));
 
-    animationController.repeat(
-        reverse: true, period: const Duration(seconds: 4));
+    // Starting the animation
+    animationController.repeat(reverse: true, period: const Duration(seconds: 4));
     animationController.forward();
-  }
-
-  goToOnBoardingPage() {
-    Future.delayed(const Duration(seconds: 5), () {
-      // ignore: use_build_context_synchronously
-      Navigator.pushNamed(context, Routes.onBoardingPath);
-    });
   }
 }
